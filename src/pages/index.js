86a843c2017019c89c37148436d6046f64599e73
Devon Campbell <raddevon@gmail.com>
2018-05-26 16:14:47 -0700

    Adds lead capture form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Intro from '../components/intro';
+import LeadForm from '../components/form';
 
 const IndexPage = ({ data }) => (
   <div>
     <Intro>{data.intro.html}</Intro>
+    <LeadForm />
   </div>
 );
 
