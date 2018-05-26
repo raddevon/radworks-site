5f285640fb41049ee98a297acaa272e6de0bc9b1
Devon Campbell <raddevon@gmail.com>
2018-05-26 16:07:50 -0700

    Adds heading

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Intro = ({ children }) => {
   return (
-    <section className="intro" dangerouslySetInnerHTML={{ __html: children }} />
+    <section className="intro">
+      <h2>Web Consulting for&nbsp;Startups and&nbsp;Bootstrappers</h2>
+      <div dangerouslySetInnerHTML={{ __html: children }} />
+    </section>
   );
 };
 
