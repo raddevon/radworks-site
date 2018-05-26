5fc01d02da3b3a9218b6e01dc0ba7f9a57d70f62
Devon Campbell <raddevon@gmail.com>
2018-05-26 16:14:26 -0700

    Generalizes name of query
    so it can fetch data for other parts of the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = ({ data }) => (
 export default IndexPage;
 
 export const query = graphql`
-  query IntroContentQuery {
+  query IndexQuery {
     intro: markdownRemark(fileAbsolutePath: { glob: "**/*/intro.md" }) {
       html
     }
