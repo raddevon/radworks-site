f6750a7bbf4660939f160c1cd9ba67b6caf6b1d0
Devon Campbell <raddevon@gmail.com>
2018-05-26 16:13:46 -0700

    Tweaks font styles

---
+++ src/components/form.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+
+import styles from './form.sass';
+
+class LeadForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.openForm = this.openForm.bind(this);
+  }
+
+  openForm() {
+    this.typeformEmbed.typeform.open();
+  }
+
+  render() {
+    return (
+      <div className="lead-form-container">
+        <ReactTypeformEmbed
+          popup={true}
+          autoOpen={false}
+          url={'https://devoncampbell.typeform.com/to/UvqwnO'}
+          hideHeaders={true}
+          hideFooter={true}
+          style={{ top: 100 }}
+          ref={tf => (this.typeformEmbed = tf)}
+        />
+
+        <div className="lead-form-button-container">
+          <h2>Ready?</h2>
+          <button onClick={this.openForm}>Start building with us</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LeadForm;
