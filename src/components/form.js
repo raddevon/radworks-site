833b15c266d3fb08c6147465a1a32630dcede036
Devon Campbell <raddevon@gmail.com>
2018-05-27 11:40:41 -0700

    Uses ReactTypeformEmbed conditionally

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { ReactTypeformEmbed } from 'react-typeform-embed';
+if (typeof window !== 'undefined') {
+  // Declared with var to scope this globally
+  var ReactTypeformEmbed = require('react-typeform-embed').ReactTypeformEmbed;
+}
 
 import styles from './form.sass';
 
@@ -9,23 +12,34 @@ class LeadForm extends React.Component {
     this.openForm = this.openForm.bind(this);
   }
 
+  renderEmbed() {
+    if (typeof ReactTypeformEmbed !== 'undefined') {
+      return (
+        <ReactTypeformEmbed
+          popup={true}
+          mode={'drawer_right'}
+          autoOpen={false}
+          url={'https://devoncampbell.typeform.com/to/UvqwnO'}
+          hideHeaders={true}
+          hideFooter={true}
+          ref={tf => (this.typeformEmbed = tf)}
+        />
+      );
+    }
+  }
+
   openForm() {
-    this.typeformEmbed.typeform.open();
+    if (typeof ReactTypeformEmbed !== 'undefined') {
+      this.typeformEmbed.typeform.open();
+    } else {
+      location.assign('https://devoncampbell.typeform.com/to/UvqwnO');
+    }
   }
 
   render() {
     return (
       <section className="lead-form-container">
-        <ReactTypeformEmbed
-          popup={true}
-          autoOpen={false}
-          url={'https://devoncampbell.typeform.com/to/UvqwnO'}
-          hideHeaders={true}
-          hideFooter={true}
-          style={{ top: 100 }}
-          ref={tf => (this.typeformEmbed = tf)}
-        />
-
+        {this.renderEmbed()}
         <div className="lead-form-button-container">
           <h2>Ready?</h2>
           <button onClick={this.openForm}>Start building with us</button>
